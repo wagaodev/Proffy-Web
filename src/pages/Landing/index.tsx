65a755182a145493ae9480e02fe58aeed66ebe5e
wcfx <wcfx.dev@gmail.com>
2020-08-03 18:30:05 -0300

    Projetando a landing page 2.0

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -20,14 +20,14 @@ export default function Landing(){
         </div>
         <img src={LandingImg} alt="Plataforma de estudos" className="hero-image" />
 
-        <div className="button-container" >
+        <div className="buttons-container" >
           <a href="#" className="study">
           <img src={studyIcon} alt=""/>
           Estudar
           </a>
         
 
-          <a href="#" className="classes">
+          <a href="#" className="give-classes">
           <img src={giveClassesIcon} alt=""/>
           Classes
           </a>
