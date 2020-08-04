3150303b3f6955798af14c827a035b9b5afce086
wcfx <wcfx.dev@gmail.com>
2020-08-04 04:19:15 -0300

    Criando as props

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 export default function TeacherForm(){
   return(
-    <h1>Teacher Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
   );
 };
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/logo.svg';
-import backIcon from '../../assets/icons/back.svg';
+import PageHeader from '../../components/PageHeader';
+
 
 import './styles.css';
 
 export default function TeacherList(){
   return(
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-          </Link>
-            <img src={logoImg} alt="Proffy"/>
-        </div>
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+      <PageHeader title="Estes são os proffys disponíveis" />
     </div>
   );
 };
